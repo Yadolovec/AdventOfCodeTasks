08eedb27ac32ec07c95edaec2d1b03253e82228a
Havrylets Volodymyr <yadolovec@gmail.com>
2023-02-03 12:13:49 +0200

    Third day first task

---
+++ src/main/java/com/some/app/EmptyApp/ThirdDay/ThirdDayTask1.java
@@ -0,0 +1,41 @@
+package com.some.app.EmptyApp.ThirdDay;
+
+import com.some.app.EmptyApp.util.Utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ThirdDayTask1 {
+    public static void main(String[] args) {
+        List<String> list = Utils.getListFromText("src/main/resources/ThirdDay.txt");
+        String[] halfs;
+        Character[] charArray1;
+        Character[] charArray2;
+        Set<Character> set;
+        Set<Character> set1;
+        Set<Character> set2;
+        int points = 0;
+
+        for (String s : list) {
+            halfs = Utils.splitInHalf(s);
+            charArray1 = Utils.toCharacterArray(halfs[0].toCharArray());
+            charArray2 = Utils.toCharacterArray(halfs[1].toCharArray());
+
+            set1 = Arrays.stream(charArray1).collect(Collectors.toSet());
+            set2 = Arrays.stream(charArray2).collect(Collectors.toSet());
+            set = new HashSet<>(set1);
+
+            set1.removeAll(set2);
+            set.removeAll(set1);
+
+            points+=Utils.getLetterPriority((char) set.toArray()[0]);
+
+        }
+        System.out.println(points);
+
+    }
+}
+
--- src/main/java/com/some/app/EmptyApp/util/Utils.java
+++ src/main/java/com/some/app/EmptyApp/util/Utils.java
@@ -3,9 +3,24 @@ package com.some.app.EmptyApp.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Utils {
+    public static List<String> getListFromText(String pathToFile){
+        List<String> list = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(pathToFile));
+            String s;
+            while ((s = br.readLine())!=null) {
+                list.add(s);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
     //FOR DAY ONE
     public static List<Integer> getListFromTextAndAddCalories(String pathToFile){
         List<Integer> list = new ArrayList<>();
@@ -28,17 +43,33 @@ public class Utils {
         return list;
     }
 
-    public static List<String> getListFromText(String pathToFile){
-        List<String> list = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(pathToFile));
-            String s;
-            while ((s = br.readLine())!=null) {
-                list.add(s);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
+    //FOR DAY THREE
+    public static int getLetterPriority(char letter){
+        int priority = 0;
+        if (letter>='a'&&letter<='z'){
+            priority=letter-96;
+        } else
+        if (letter>='A'&&letter<='Z'){
+            priority=letter-65+27;
         }
-        return list;
+        return priority;
     }
+
+    public static String[] splitInHalf(String s){
+        String[] toReturn = new String[2];
+        int size = s.length();
+        toReturn[0]=s.substring(0, size/2);
+        toReturn[1]=s.substring(size/2, size);
+        return toReturn;
+    }
+
+    public static Character[] toCharacterArray(char[] array){
+        Character[] toReturn = new Character[array.length];
+        for (int i = 0; i< array.length; i++){
+            toReturn[i] = array[i];
+        }
+        return toReturn;
+    }
+
+
 }
