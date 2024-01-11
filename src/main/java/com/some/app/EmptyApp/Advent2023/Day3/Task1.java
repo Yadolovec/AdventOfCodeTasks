5f4d2d90c27b36b07ea9644628fbcf8a42611198
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-11 18:08:07 +0200

    day 3 task 1, unfinished

---
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day3/Task1.java
@@ -0,0 +1,119 @@
+package com.some.app.EmptyApp.Advent2023.Day3;
+
+import com.some.app.EmptyApp.util.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Volodymyr Havrylets
+ * @link volodymyr.havrylets@embrox.com
+ * @since 11.01.2024
+ **/
+public class Task1 {
+    public static void main(String[] args) {
+        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day3t");
+
+        List<Number> numbers = new ArrayList<>();
+        List<Coordinates> symbolsCoordinates = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            char[] charArray = s.toCharArray();
+
+            int start = 0;
+            int end = 0;
+            boolean numberOperation = false;
+
+            for (int j = 0; j < charArray.length; j++) {
+                char c = charArray[j];
+
+                if (!Character.isDigit(c) && c != '.') {
+                    symbolsCoordinates.add(new Coordinates(j, i));
+                }
+
+                if (Character.isDigit(c) && !numberOperation) {
+                    start = j;
+                    numberOperation = true;
+                }
+
+                if (!Character.isDigit(c) && numberOperation) {
+                    end = j - 1;
+                    numberOperation = false;
+
+                    System.out.println(start + " " + end);
+                    int value = Integer.parseInt(s.substring(start, end));
+
+                    numbers.add(new Number(new Coordinates(start, i),
+                            end - start + 1,
+                            value));
+                }
+            }
+        }
+    }
+
+}
+
+
+class Number {
+    private Coordinates beginCoordinates;
+    private int length;
+    private int value;
+
+    public Number(Coordinates beginCoordinates, int length, int value) {
+        this.beginCoordinates = beginCoordinates;
+        this.length = length;
+        this.value = value;
+    }
+
+    public Coordinates getBeginCoordinates() {
+        return beginCoordinates;
+    }
+
+    public void setBeginCoordinates(Coordinates beginCoordinates) {
+        this.beginCoordinates = beginCoordinates;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
+
+class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
