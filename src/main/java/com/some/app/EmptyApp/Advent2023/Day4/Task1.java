434f428bb3f4c173b625dcd13069f191b315c326
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-16 12:42:11 +0200

    day 4 task 1 done

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day4/Task1.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day4/Task1.java
@@ -1,9 +1,55 @@
 package com.some.app.EmptyApp.Advent2023.Day4;
 
+import com.some.app.EmptyApp.util.Utils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * @author Volodymyr Havrylets
  * @link volodymyr.havrylets@embrox.com
  * @since 15.01.2024
  **/
 public class Task1 {
+
+    public static void main(String[] args) {
+        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day4");
+        list = list.stream().map(x -> x.split(":")[1]).collect(Collectors.toList());
+
+        int answer = 0;
+        for (String line : list) {
+            int powerAnswer = -1;
+            String[] sArray = line.split("\\|");
+            Set<Integer> winningNumbersSet = new HashSet<>(extractNumbers(sArray[0]));
+            List<Integer> numbers = extractNumbers(sArray[1]);
+
+            for (Integer i : numbers) {
+                if (winningNumbersSet.contains(i)) {
+                    powerAnswer += 1;
+                }
+            }
+
+            answer += powerAnswer == -1 ? 0 : Math.pow(2, powerAnswer);
+
+        }
+
+        System.out.println(answer);
+
+
+    }
+
+    public static List<Integer> extractNumbers(String s) {
+        List<Integer> toReturn = new ArrayList<>();
+        String[] sArray = s.split(" ");
+        for (String number : sArray) {
+            if (!number.equals("") && !number.equals(" ")) {
+                toReturn.add(Integer.parseInt(number));
+            }
+        }
+
+        return toReturn;
+    }
 }
