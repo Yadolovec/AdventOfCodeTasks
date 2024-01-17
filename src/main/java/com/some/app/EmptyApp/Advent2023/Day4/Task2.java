36c177c2da025dbcc112e8f4c6e933fac8ca2590
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-17 15:08:06 +0200

    day 4 task 2 test working actual data doesnt

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day4/Task2.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day4/Task2.java
@@ -2,10 +2,7 @@ package com.some.app.EmptyApp.Advent2023.Day4;
 
 import com.some.app.EmptyApp.util.Utils;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -16,25 +13,44 @@ import java.util.stream.Collectors;
 public class Task2 {
 
     public static void main(String[] args) {
-        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day4t");
+        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day4");
         list = list.stream().map(x -> x.split(":")[1]).collect(Collectors.toList());
 
-        int answer = 0;
-        for (String line : list) {
-            int powerAnswer = -1;
+        Map<Integer, Integer> repeatMap = new HashMap<>();
+        repeatMap.put(0, 0);
+
+        for (int i = 0; i < list.size(); i++) {
+            int quantityOfWinnings = 0;
+            String line = list.get(i);
             String[] sArray = line.split("\\|");
             Set<Integer> winningNumbersSet = new HashSet<>(extractNumbers(sArray[0]));
             List<Integer> numbers = extractNumbers(sArray[1]);
 
-            for (Integer i : numbers) {
-                if (winningNumbersSet.contains(i)) {
-                    powerAnswer += 1;
+            for (Integer j : numbers) {
+                if (winningNumbersSet.contains(j)) {
+                    quantityOfWinnings++;
                 }
             }
 
-            answer += powerAnswer == -1 ? 0 : Math.pow(2, powerAnswer);
+            if (!repeatMap.containsKey(i)){
+                repeatMap.put(i, 0);
+            }
 
+            for (int k = i + 1; k <= i + quantityOfWinnings; k++) {
+                if (repeatMap.containsKey(k)) {
+                    repeatMap.put(k, repeatMap.get(k) + repeatMap.get(i) + 1);
+                } else {
+                    if (k < list.size())
+                        repeatMap.put(k, 1);
+                }
+            }
         }
+
+        int answer = list.size() + repeatMap
+                .values()
+                .stream()
+                .reduce(Integer::sum)
+                .get();
 
         System.out.println(answer);
 
