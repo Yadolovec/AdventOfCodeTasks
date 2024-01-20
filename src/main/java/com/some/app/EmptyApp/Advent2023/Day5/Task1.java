9aaaced014a03d94f8b6f8ccbe260c44ee97cb51
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-20 14:46:51 +0200

    day 5 task 1 unfinished, method for extracting numbers and seeds;

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task1.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task1.java
@@ -2,6 +2,7 @@ package com.some.app.EmptyApp.Advent2023.Day5;
 
 import com.some.app.EmptyApp.util.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,17 +18,44 @@ public class Task1 {
         int from = 0;
         int to = 0;
 
+        Map<Integer, Integer> changeLine;
+        List<Integer> seeds = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
 
             String s = list.get(i);
             if (s.contains(":") && i != from) {
                 to = i;
+                if (from == 0) {
+                    seeds = extractNumbers(list, from, to);
+                } else {
+                    changeLine = extractChangeLine(list, from + 1, to - 2);
+                }
             }
 
-            Map<Integer, Integer> changeLine = extractChangeLine(list, from + 1, to - 2);
             from = to;
-
-
         }
+
+        System.out.println(" ");
+    }
+
+    public static Map<Integer, Integer> extractChangeLine(List<String> list, int from, int to) {
+
+        System.out.println("");
+        return null;
+    }
+
+    public static List<Integer> extractNumbers(List<String> list, int from, int to) {
+        List<Integer> toReturn = new ArrayList<>();
+
+        for (int i = from; i <= to; i++) {
+            String[] sArray = list.get(i).split(" ");
+            for (String s1 : sArray) {
+                if (s1.chars().allMatch(Character::isDigit) && !s1.isEmpty()) {
+                    toReturn.add(Integer.parseInt(s1));
+                }
+            }
+        }
+
+        return toReturn;
     }
 }
