53e7282e8e58c5df05c8f83bf6ddd812d31d130a
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-20 12:33:33 +0200

    day 5 task 1 unfinished

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task1.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task1.java
@@ -1,9 +1,33 @@
 package com.some.app.EmptyApp.Advent2023.Day5;
 
+import com.some.app.EmptyApp.util.Utils;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Volodymyr Havrylets
  * @link volodymyr.havrylets@embrox.com
  * @since 19.01.2024
  **/
 public class Task1 {
+    public static void main(String[] args) {
+        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day5t");
+
+        int from = 0;
+        int to = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+
+            String s = list.get(i);
+            if (s.contains(":") && i != from) {
+                to = i;
+            }
+
+            Map<Integer, Integer> changeLine = extractChangeLine(list, from + 1, to - 2);
+            from = to;
+
+
+        }
+    }
 }
