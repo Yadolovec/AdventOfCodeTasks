65de7e5a6ddb565f86ca34dc592baf9cb3bdbee9
Havrylets Volodymyr <yadolovec@gmail.com>
2024-01-29 20:08:31 +0200

    day 5 task 2 unfinished

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task2.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day5/Task2.java
@@ -1,5 +1,10 @@
 package com.some.app.EmptyApp.Advent2023.Day5;
 
+import com.some.app.EmptyApp.util.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Volodymyr Havrylets
  * @link volodymyr.havrylets@embrox.com
@@ -7,6 +12,11 @@ package com.some.app.EmptyApp.Advent2023.Day5;
  **/
 public class Task2 {
     public static void main(String[] args) {
+        List<String> list = Utils.getListFromText("src/main/resources/Res2023/Day5");
 
+        int from = 0;
+        int to = 0;
+
+        List<Long> seeds = new ArrayList<>();
     }
 }
