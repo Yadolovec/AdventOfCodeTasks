36758aee55d2ab2ff0e7355e634dc650f4eff079
Havrylets Volodymyr <yadolovec@gmail.com>
2024-02-26 18:19:03 +0200

    day 10 task 1 unfinished, wrong answer

--- src/main/java/com/some/app/EmptyApp/Advent2023/Day10/Task1.java
+++ src/main/java/com/some/app/EmptyApp/Advent2023/Day10/Task1.java
@@ -2,19 +2,13 @@ package com.some.app.EmptyApp.Advent2023.Day10;
 
 import com.some.app.EmptyApp.util.Utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Volodymyr Havrylets
  * @since 22.02.2024
  **/
 public class Task1 {
-    enum Direction {WEST, EAST, SOUTH, NORTH, NONE}
-
-    ;
 
 
     public static void main(String[] args) {
@@ -38,44 +32,65 @@ public class Task1 {
             }
         }
 
-        Map<Integer, int[]> way = new HashMap<>();
-        int[] coordinates = start;
+        int[] tile = start;
         int i = 0;
-        Direction ignoredDirection = Direction.NONE;
+        int[] previousTile = start.clone();
         do {
-            way.put(i, coordinates);
             i++;
-            coordinates = calculateNext(coordinates, map, ignoredDirection);
+            tile = calculateNext(tile, map, previousTile);
+            previousTile = tile.clone();
+        } while (Arrays.equals(tile, start));
 
-
-        } while (coordinates.equals(start));
+        System.out.println(i);
 
 
     }
 
-    public static int[] calculateNext(int[] coordinates, Map<Integer, char[]> map, Direction ignoredDirection) {
+    public static int[] calculateNext(int[] tile, List<char[]> map, int[] previousTile) {
 
-        if (ignoredDirection == Direction.NONE) {
 
+        int[] toReturn = {-1, -1};
+
+        int[][] potentialNextTiles = new int[4][2];
+
+        potentialNextTiles[0] = new int[]{tile[0], tile[1] + 1};
+        potentialNextTiles[1] = new int[]{tile[0], tile[1] - 1};
+        potentialNextTiles[2] = new int[]{tile[0] + 1, tile[1]};
+        potentialNextTiles[3] = new int[]{tile[0] - 1, tile[1]};
+
+        for (int [] nextTile : potentialNextTiles){
+            char tileName = getTileByCoordinates(map, tile);
+            char nextTileName = getTileByCoordinates(map, nextTile);
+
+            if (isConnectable(tileName, nextTileName) && !Arrays.equals(nextTile, previousTile)){
+                toReturn = nextTile;
+                break;
+            }
         }
 
-        char west = coordinates[1] - 1 >= 0
-                ? map.get(coordinates[0])[coordinates[1] - 1] : ' ';
-        char east = coordinates[1] + 1 < map.get(0).length
-                ? map.get(coordinates[0])[coordinates[1] + 1] : ' ';
-        char north = coordinates[0] - 1 >= 0
-                ? map.get(coordinates[0] - 1)[coordinates[1]] : ' ';
-        char south = coordinates[1] - 1 < map.size()
-                ? map.get(coordinates[0] + 1)[coordinates[1]] : ' ';
-
-
-        char currentLocation = map.get(coordinates[0])[coordinates[1]];
-        if (currentLocation == 'S')
+        return toReturn;
 
     }
-
-    public class Tile {
-
+    public static char getTileByCoordinates(List<char[]> map, int[] coordinates){
+        return map.get(coordinates[0])[coordinates[1]];
     }
+
+    public static boolean isConnectable(char first, char second){
+        if (first == 'S' || second == 'S')
+            return true;
+
+        if (second == '.')
+            return false;
+
+        if (first == '|')
+            return  second != '-';
+
+        if (first == '-')
+            return second != '|';
+
+        return !(first == second);
+    }
+
+
 }
 
